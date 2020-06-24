6c91b088b49dd107e207a37825c52043902208e6
Ana Marr <acarolinasim@gmail.com>
2020-06-24 11:09:24 +0930

    gitgnore

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require("express");
+var compression = require('compression')
 
 var PORT = process.env.PORT || 3000;
 
-var app = express();
+ 
+var app = express()
+ 
+// compress all responses
+app.use(compression())
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
